4e4e670dc5fd5b04acdcab435142c3c7d3674df7
PedroHenrrique17 <pedro.henrriquee17@gmail.com>
2026-05-25 16:31:03 -0300

    listar

--- curso-fullstack/node-people/index.js
+++ curso-fullstack/node-people/index.js
@@ -1,12 +1,23 @@
 const express = require("express")
 const app = express();
 
+app.use(express.json());
 const PORT = 300;
 
-app.listen(PORT, () => {
-    console.log(`Servidor do projeto, rodando em http://localhost:${PORT}`);
-})
+// mock
+const nomes = [
+    { id: 1, nome: "Fernanda", idade: 18, casado: true },
+    { id: 2, nome: "Juliana", idade: 22, casado: false },
+    { id: 3, nome: "Doris", idade: 15, casado: false }
+];
 
+// Criando funções auxiliares
+//Retornar o objeto pot ID 
+function buscarNomePorID(id) {
+    return nomes.filter((nome) => nome.id == id);
+}
+
+//Rota principal
 app.get('/', (req, res) => {
     res.send("Seja bem vindo");
 });
@@ -14,3 +25,27 @@ app.get('/', (req, res) => {
 app.get('/teste', (req, res) => {
     res.send("Testando");
 });
+
+// Buscando nomes (listaNomes)
+app.get('/listaNomes', (req, res) => {
+    res.send(nomes)
+});
+
+//Buscando por ID
+app.get("/listaNomes/:id", (req, res) => {
+    let index = req.params.id;
+
+    res.json(buscarNomePorID(index));
+});
+
+// Criando um cadastro
+app.post("/listaNomes", (req, res) => {
+    nomes.push(req.body)
+    res.status(201).send('Nomes cadastrado com sucesso!');
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor do projeto, rodando em http://localhost:${PORT}`)
+});
+
